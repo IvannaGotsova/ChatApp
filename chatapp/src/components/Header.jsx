0d4d07fdc3ec2adb381adab0ec28a1f0cbfd73ec
IvannaGotsova <ivannagotsova@outlook.com>
2025-04-10 17:41:02 +0300

    Added Header file

---
+++ chatapp/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import styles from '../css/Header.module.css'
+
+function Header () {
+    return (
+        <>
+          <h1 className={styles.headerStyle}>CHAT APP</h1>
+        </>
+    )
+}
+
+export default Header
