6f5e0b201be55fb8b85b5604cb71d11fa599e711
IvannaGotsova <ivannagotsova@outlook.com>
2025-04-11 19:29:52 +0300

    Added components

--- chatapp/src/App.jsx
+++ chatapp/src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
+import Body from './components/Body'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
-      
+      <Header />
+      <br /><br /><br />
+      <Body />
+      <br /><br /><br />
+      <Footer />
     </>
   )
 }
