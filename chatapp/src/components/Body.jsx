5fbe78994f21b33fa5c3cca496fa818f9249e0a1
IvannaGotsova <ivannagotsova@outlook.com>
2025-04-11 19:30:15 +0300

    Added buttons

---
+++ chatapp/src/components/Body.jsx
@@ -0,0 +1,19 @@
+import styles from '../css/Body.module.css'
+
+function Body () {
+    return (
+        <>
+          <div>
+            <input/>
+            <br />
+            <button type="button">Add Chat</button>
+            <br />
+            <br />
+            <br />
+            <button type="button">All Chats</button>
+          </div>
+        </>
+    )
+}
+
+export default Body
