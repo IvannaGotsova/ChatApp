f8e75f6449bbd1ec32cf1e6a2478058d118260c0
IvannaGotsova <ivannagotsova@outlook.com>
2025-04-13 13:20:29 +0300

    Added basic routing

--- chatapp/src/App.jsx
+++ chatapp/src/App.jsx
@@ -1,7 +1,10 @@
 import './App.css'
+import { Routes, Route } from "react-router-dom";
 import Body from './components/Body'
 import Footer from './components/Footer'
 import Header from './components/Header'
+import NewChat from './components/NewChat';
+import AllUsers from './components/AllUsers';
 
 function App() {
 
@@ -12,6 +15,13 @@ function App() {
       <Body />
       <br /><br /><br />
       <Footer />
+
+              
+      <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="new-chat" element={<NewChat />} />
+            <Route path="all-users" element={<AllUsers />} />
+      </Routes>
     </>
   )
 }
