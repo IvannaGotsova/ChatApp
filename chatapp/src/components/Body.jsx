01f65da0683db68e58ea97cfe16525941bbe8970
IvannaGotsova <ivannagotsova@outlook.com>
2025-04-15 21:36:36 +0300

    Modified Body.jsx

--- chatapp/src/components/Body.jsx
+++ chatapp/src/components/Body.jsx
@@ -1,26 +1,18 @@
 import styles from '../css/Body.module.css'
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Body () {
+
+  const navigate = useNavigate()
+  
     return (
         <>
-          <div>
-            <input placeholder='Add New Chat Title'/>
-            <br />
-            <button type="button">Add Chat</button>
-            <br />
-            <br />
-            <br />
-            <button type="button">All Chats</button>
-            <h3>Test Chat</h3>
-            <h3>Test Chat</h3>
-            <h3>Test Chat</h3>
-            <h3>Test Chat</h3>
-            <h3>Test Chat</h3>
-            <h3>Test Chat</h3>
-            <h3>Test Chat</h3>
-            <h3>Test Chat</h3>
-            <h3>Test Chat</h3>
-            <h3>Test Chat</h3>
+          <div className={styles.bodyStyle}>
+            <h3>Start a chat with online users</h3>
+            <button onClick={() => navigate('/new-chat')}>New Chat</button>
+            <h3>See all chat app users</h3>
+            <button onClick={() => navigate('/all-users')}>All Users</button>
           </div>
         </>
     )
