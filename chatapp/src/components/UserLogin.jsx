23734b17bd2c33eed1c2f8a31c863be0b1e6b5aa
IvannaGotsova <ivannagotsova@outlook.com>
2025-04-21 21:55:19 +0300

    Added provider

--- chatapp/src/components/UserLogin.jsx
+++ chatapp/src/components/UserLogin.jsx
@@ -1,39 +1,59 @@
-import React, { useState } from "react"
+import React, { useState, createContext, useContext, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import users from "../db//users.json"
+import Body from "./Body";
 
-const UserLogin = () => {
+const UserContext = createContext();
+
+function UserLogin () {
+
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
-  const [statusUser, setStatusUserUser] = useState(false)
+  const [statusUser, setStatusUser] = useState(false)
+  const [loginUser, setLoginUser] = useState(false)
   const [error, setError] = useState("")
   const navigate = useNavigate()
 
   const onClickAfterLogin = () => {
+    
     const findUser = users.find((u) => u.user === username && u.password.toString() === password)
 
     if (findUser) {
-      navigate("/")
-      setError("")
+      navigate("/index")
+      setError("SUCCESS")
       setStatusUser(true)
+      setLoginUser(true)
+      console.log(findUser.login)
     } else {
-      setError("Invalid User")
+      setError("INVALID USER")
       setStatusUser(false)
     }
+
   }
+
+  useEffect(() => {
+    console.log("StatusUser updated:", statusUser)
+    console.log("LoginUser updated:", loginUser)
+  }, [statusUser, loginUser])
+
 
   return (
     <>
-      <h3>Username and password are required</h3>
-      <input type="text" placeholder="Your Username" value={username} onChange={(e) => setUsername(e.target.value)}/>
-      <br />
-      <input type="password" placeholder="Your Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-      <br />
-      <button onClick={onClickAfterLogin}>Login</button>
-      <br />
-      {error}
+      <UserContext.Provider value={loginUser}>
+        <h3>Username and password are required</h3>
+        <input type="text" placeholder="Your Username" value={username} onChange={(e) => setUsername(e.target.value)}/>
+        <br />
+        <input type="password" placeholder="Your Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+        <br /><br />
+        <button onClick={onClickAfterLogin}>Login</button>
+        <br />
+        {error}
+      </UserContext.Provider>
     </>
   )
+
 }
+
+
 
 export default UserLogin
