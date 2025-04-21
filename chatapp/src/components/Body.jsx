21c7d0477d8fc4ca109fcfb0aa6a29ae207d8963
IvannaGotsova <ivannagotsova@outlook.com>
2025-04-21 21:55:31 +0300

    Added provider

--- chatapp/src/components/Body.jsx
+++ chatapp/src/components/Body.jsx
@@ -1,18 +1,24 @@
 import styles from '../css/Body.module.css'
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router-dom"
+import { createContext, useContext } from 'react';
+import { Navigate  } from 'react-router-dom'
+
+const UserContext = createContext();
 
 function Body () {
 
+  const userLogin = useContext(UserContext)
+  console.log(userLogin)
   const navigate = useNavigate()
   
     return (
         <>
           <div className={styles.bodyStyle}>
-            <h3>Start a chat with online users</h3>
-            <button onClick={() => navigate('/new-chat')}>New Chat</button>
-            <h3>See all chat app users</h3>
-            <button onClick={() => navigate('/all-users')}>All Users</button>
+            {userLogin === true ? <h3>Start a chat with online users</h3> : <Navigate to='/error'/>}
+            {userLogin === true ? <button onClick={() => navigate('/new-chat')}>New Chat</button> : <Navigate to='/error'/>}
+            {userLogin === true ? <h3>See all chat app users</h3> : <Navigate to='/error'/>}
+            {userLogin === true ? <button onClick={() => navigate('/all-users')}>All Users</button> : <Navigate to='/error'/>}
           </div>
         </>
     )
