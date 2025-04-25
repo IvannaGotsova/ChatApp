05969245e8d6499fa6ad43426d62e4d91f254b48
IvannaGotsova <ivannagotsova@outlook.com>
2025-04-25 16:57:44 +0300

    Added users

--- chatapp/src/components/NewChat.jsx
+++ chatapp/src/components/NewChat.jsx
@@ -1,7 +1,25 @@
+import users from "../db//users.json"
+import {Popup} from 'reactjs-popup';
+import { useNavigate } from "react-router-dom"
+
 function NewChat () {
     return (
         <>
           <h1>New Chat</h1>
+
+          {users.map(u => {
+
+            return (
+              <div key={u.id}>
+                {u.status === true ? <h3 key={u.id}> {u.user}</h3> : " "}
+                {u.status === true ? <h5>{u.status == true ? "online" : "offline"}</h5> : " "}
+                {u.status === true ? <button onClick={() => navigate('/new-chat')} disabled={u.status == false}>Chat</button> : " "}
+              </div>
+            )
+          }
+
+          )}
+          
         </>
     )
 }
