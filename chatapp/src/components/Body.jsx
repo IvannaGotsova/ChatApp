3993033c9dd7ec4670a5fda9e6a847bed12d77d2
IvannaGotsova <ivannagotsova@outlook.com>
2025-04-29 12:42:35 +0300

    Added a CONST

--- chatapp/src/components/Body.jsx
+++ chatapp/src/components/Body.jsx
@@ -1,25 +1,24 @@
 import styles from '../css/Body.module.css'
 import React from 'react'
 import { useNavigate } from "react-router-dom"
-import { createContext, useContext } from 'react';
 import { Navigate  } from 'react-router-dom'
+import users from "../db//users.json"
 
-const UserContext = createContext();
 
 function Body () {
 
-  const userLogin = useContext(UserContext)
-  console.log(userLogin)
   const navigate = useNavigate()
-  
+
     return (
         <>
+        {window.LOGINUSER === true ? 
           <div className={styles.bodyStyle}>
-            {userLogin === true ? <h3>Start a chat with online users</h3> : <Navigate to='/error'/>}
-            {userLogin === true ? <button onClick={() => navigate('/new-chat')}>New Chat</button> : <Navigate to='/error'/>}
-            {userLogin === true ? <h3>See all chat app users</h3> : <Navigate to='/error'/>}
-            {userLogin === true ? <button onClick={() => navigate('/all-users')}>All Users</button> : <Navigate to='/error'/>}
-          </div>
+            <h3>Start a chat with online users</h3> 
+            <button onClick={() => navigate('/new-chat')}>New Chat</button> 
+            <h3>See all chat app users</h3> 
+            <button onClick={() => navigate('/all-users')}>All Users</button> 
+          </div> 
+        : <Navigate to='/'/>}
         </>
     )
 }
