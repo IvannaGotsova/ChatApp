cbe97d3f34fc6b594da0031749c4933e4d9288fb
IvannaGotsova <ivannagotsova@outlook.com>
2025-05-02 13:41:09 +0300

    Added CONST

--- chatapp/src/components/NewChat.jsx
+++ chatapp/src/components/NewChat.jsx
@@ -1,12 +1,19 @@
 import users from "../db//users.json"
 import {Popup} from 'reactjs-popup';
 import { useNavigate } from "react-router-dom"
+import { Navigate  } from 'react-router-dom'
 
 function NewChat () {
+
+   const navigate = useNavigate()
+
     return (
         <>
+        {console.log(window.LOGINUSER)}
+        {window.LOGINUSER === true ? 
+        <div>
           <h1>New Chat</h1>
-
+  
           {users.map(u => {
 
             return (
@@ -14,12 +21,12 @@ function NewChat () {
                 {u.status === true ? <h3 key={u.id}> {u.user}</h3> : " "}
                 {u.status === true ? <h5>{u.status == true ? "online" : "offline"}</h5> : " "}
                 {u.status === true ? <button onClick={() => navigate('/new-chat')} disabled={u.status == false}>Chat</button> : " "}
-              </div>
+              </div>              
             )
           }
-
           )}
-          
+        </div>
+        : <Navigate to='/'/>}
         </>
     )
 }
