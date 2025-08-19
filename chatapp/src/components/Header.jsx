1ddcf7f85cf0c7ef2174c346287d9f88c24648eb
IvannaGotsova <ivannagotsova@outlook.com>
2025-08-19 10:14:17 +0300

    Added Home button in the header

--- chatapp/src/components/Header.jsx
+++ chatapp/src/components/Header.jsx
@@ -3,7 +3,7 @@ import styles from '../css/Header.module.css'
 function Header () {
     return (
         <>
-          <h1 className={styles.headerStyle}>CHAT APP</h1>
+          <h1 className={styles.headerStyle}><button className={styles.chatButton} onClick={() => window.open(`${window.location.origin}/index`, '_blank')}>HOME</button>CHAT APP</h1>
         </>
     )
 }
